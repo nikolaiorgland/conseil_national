c0a97847635227129f35fd1c29997c2153691a59
Silvan Stettler <silvan.stettler@epfl.ch>
2018-10-15 23:40:56 +0200

    Data import and rearrange done. 'features' dataframe ready

--- visualization/create_network.py
+++ visualization/create_network.py
@@ -17,10 +17,27 @@ def assign_val_to_vote(data):
     return counc_votes
     #return counc_votes[['AffairShortId','value']]
 
-def join_votes_to_affairs(affair_idx, val_votes, councId):
-    val_votes = val_votes.set_index('AffairShortId')
-    affair_vote_idx = affair_idx.join(val_votes, on='AffairShortId')
-    affair_vote_idx = affair_vote_idx.rename(columns={'value':councId})
-    return affair_vote_idx
+def extract_feature_vec(val_votes, councId, complete_feat_idx):
+    # Look for the votes of one particular councillor and keep values and affair Ids
+    feature_vec = val_votes.loc[val_votes.CouncillorId == councId, :]
+    feature_vec = feature_vec.drop(columns=['CouncillorId'])
+    
+    # For multiple votes with the same Id, only keep the deciding vote (the first one in the df)
+    feature_vec = feature_vec[~feature_vec.index.duplicated(keep='first')]
+    
+    # Councillors that have missing data for an affair get assigned NA in the feature vector
+    # (Which can be the case for people who resign for example)
+    feature_vec = complete_feat_idx.join(feature_vec)
+    
+    # Rename the value column to have the councillor Id as name for identification later on
+    feature_vec = feature_vec.rename(columns={'value':str(councId)})
+    return feature_vec
+
+
+
+def downcast_column(df, column, dtype):
+    return pd.to_numeric(df[column], downcast=dtype)
+
+
     
     
